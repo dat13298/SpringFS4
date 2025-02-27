a6669d0d8922b81df42570f04f5aa9e030f80ed0
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-02-27 23:22:26 +0700

    first commit

---
+++ src/main/java/com/aptech/springfs4/SpringFs4Application.java
@@ -0,0 +1,13 @@
+package com.aptech.springfs4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFs4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFs4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/aptech/springfs4/SpringFs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptech.springfs4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFs4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
