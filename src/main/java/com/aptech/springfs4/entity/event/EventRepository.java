0b77efc8db742ac3f27111cdb86d83ceb53ccf13
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-02-28 01:07:19 +0700

    add entity EventRepository

---
+++ src/main/java/com/aptech/springfs4/entity/event/EventRepository.java
@@ -0,0 +1,8 @@
+package com.aptech.springfs4.entity.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
